4ea7586fb016f132715a59e89731e6b9c1352d3c
Pool Laptop <poollaptop@AND0423.local>
2020-01-27 10:29:14 +0000

    Made buttons better

--- src/components/settingsPanel.tsx
+++ src/components/settingsPanel.tsx
@@ -3,13 +3,14 @@ import { makeStyles, Theme, createStyles } from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
-import MenuItem from '@material-ui/core/MenuItem'
-import ListItemText from '@material-ui/core/ListItemText';
-import DoubleArrowIcon from '@material-ui/icons/DoubleArrow';
 import { Box } from '@material-ui/core';
 import AppContext from '../AppContext';
 import SpeedSlider from './speedSlider';
 import SettingsCheckbox from './settingsCheckbox'
+import Radio from '@material-ui/core/Radio';
+import RadioGroup from '@material-ui/core/RadioGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -31,7 +32,10 @@ const useStyles = makeStyles((theme: Theme) =>
     selected: {
       backgroundColor: theme.palette.primary.main,
       color: theme.palette.common.white,
-    }
+    },
+    formControl: {
+      margin: theme.spacing(3),
+    },
   }),
 );
 
@@ -40,32 +44,36 @@ export default function SettingsPanel() {
 
   const { dispatch } = useContext<AppContext>(AppContext)
 
-  const [selected, setSelected] = useState<number>(0)
+  const [selected, setSelected] = useState<string>('Dijkstra')
 
-  const dispatchAlgorithm = (text: string, index: number) => {
-    setSelected(index)
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const text: string = (event.target as HTMLInputElement).value
+    setSelected(text);
     dispatch({ currentAlgorithm: text })
-  }
+  };
 
   return (
     <>
       <div className={classes.menuSectionContainer} >
         <Typography variant={'h6'} >Algorithm</Typography>
         <List>
-          {["Dijkstra", "A*"].map((text, index) => (
-            <MenuItem
-              classes={{
-                root: classes.root,
-                selected: classes.selected
-              }}
-              selected={selected === index} button key={text} onClick={() => dispatchAlgorithm(text, index)}>
-              {selected === index ? <DoubleArrowIcon /> : ''}
-              <ListItemText primary={text} />
-            </MenuItem>
-          ))}
+          <Box pl={2}>
+            <FormControl component="fieldset" >
+              <RadioGroup aria-label="gender" name="gender1" value={selected} onChange={handleChange}>
+                {["Dijkstra", "A*"].map((text, index) => (
+                  <FormControlLabel
+                    value={text}
+                    control={<Radio color="primary" />}
+                    label={text}
+                    disabled={text === 'A*' ? true : false}
+                  />
+                ))}
+              </RadioGroup>
+            </FormControl>
+          </Box>
         </List>
       </div>
-      <Divider variant="middle"/>
+      <Divider variant="middle" />
       <div className={classes.menuSectionContainer} >
         <Box pl={1}>
           <Typography variant={'h6'}>Options</Typography>
